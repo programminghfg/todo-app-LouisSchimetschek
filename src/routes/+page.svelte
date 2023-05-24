576263c0ea030a02dc2e3858ec3ba9815290ef06
louis <louis.schimetschek@hfg-gmuend.de>
2023-05-24 13:00:48 +0200

    Lexikon Eintrag

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,147 +1,210 @@
-<script>
-  import { onMount } from 'svelte';
-  import { browser } from '$app/environment';
-    import ToDo from '../lib/components/ToDo.svelte';
-  
-
-  let todoText = '';
-  let todos = [];
-
-  onMount(() => {
-    if (browser) {
-      const storedTodos = JSON.parse(localStorage.getItem('todos'));
-      if (storedTodos) {
-        todos = storedTodos;
-      }
-    }
-  });
-
-  function saveTodos() {
-    if (browser) {
-      localStorage.setItem('todos', JSON.stringify(todos));
-    }
-  }
-
-  function addTodo() {
-    todos.push({ text: todoText, done: false });
-    todos = todos;
-    todoText = '';
-    saveTodos();
-  }
-
-  function remove(index) {
-    todos.splice(index, 1);
-    todos = todos;
-    saveTodos();
-  }
+<!-- <script>
+  export let name;
 </script>
 
-
-
-<h1>TODO APP</h1>
-
-<input type="text" class="todo-input" bind:value={todoText} />
-<button class="add-button" on:click={addTodo}>ADD</button>
-
-{#each todos as todo, index}
-    <ToDo {todo} {index} on:remove={(event) => remove(event.detail)}></ToDo>
-{/each}
-
-
 <style>
-  h1 {
-    font-size: 24px;
-    font-weight: bold;
-    text-align: center;
-    margin-bottom: 20px;
+  @import url('https://fonts.googleapis.com/css2?family=Rubik+Pixels&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Mono&display=swap');
+
+  @font-face {
+    font-family: 'RubikPixels-Regular';
+    src: url(fonts/RubikPixels-Regular.woff);
   }
 
-  
-  .todo-input {
-    width: 100%;
-    padding: 10px;
-    font-size: 16px;
-    border: 1px solid #ccc;
+  @font-face {
+    font-family: 'IBM Plex Mono', monospace;
+    src: url(fonts/SF-Mono-Regular.woff);
+  }
+
+  * {
+    box-sizing: border-box;
+    font-family: 'IBM Plex Mono', monospace;
+    background-color: rgb(255, 255, 255);
+  }
+
+  a {
+    color: rgb(0, 0, 0);
     border-radius: 4px;
-    margin-bottom: 10px;
-    transition: border-color 0.3s ease; /* Add this line */
-  }
-
-  .todo-input:hover {
-    border-color: #888; /* Add this block */
-  }
-
-  .add-button {
-    display: block;
-    width: 100%;
-    padding: 10px;
-    font-size: 16px;
-    background-color: #7845a0;
-    color: white;
-    border: none;
-    border-radius: 4px;
-    cursor: pointer;
-  }
-
-  .add-button:hover {
-    background-color: #7845a0;
-  }
-
-  .todo-entry {
-    display: flex;
-    align-items: center;
-    margin-bottom: 10px;
-    text-decoration: none; /* Add this line */
-  }
-  .todo-entry.done {
-    text-decoration: line-through; /* Add this block */
-  }
-  .todo-text {
-    flex: 1;
-    font-size: 16px;
-    margin-right: 10px;
-  }
-
-  .todo-done {
-    margin-right: 10px;
-  }
-
-  .delete-button {
-    background: none;
-    border: none;
-    color: #f44336;
-    font-size: 16px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    cursor: pointer;
-    transition: color 0.3s ease;
     position: relative;
-    overflow: hidden;
-    width: 24px;
-    height: 24px;
-    border-radius: 50%;
+    top: 2em;
+    right: 100px;
+    margin: auto;
+    position: fixed;
   }
 
-  .delete-button:hover {
-    color: #ff0000;
+  .wrapper {
+    display: grid;
+    width: 800px;
+    gap: 40px;
+    margin: auto;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    grid-template-rows: 75px 80px 300px 100px 150px 50px;
+    grid-template-areas:
+      "header header header header"
+      "nav nav nav nav"
+      "aside aside orange orange"
+      "aside aside orange orange"
+      "footer footer footer footer";
   }
 
-  .delete-button::before {
-    content: "";
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    width: 100%;
-    height: 100%;
-    border-radius: 50%;
-    background-color: #ccc;
-    opacity: 0;
-    transition: opacity 0.3s ease;
+  header {
+    background-color: transparent;
+    grid-area: header;
   }
 
-  .delete-button:hover::before {
-    opacity: 1;
+  nav {
+    background-color: transparent;
+    grid-area: nav;
+    display: flex;
   }
+
+  aside {
+    background-color: transparent;
+    grid-area: aside;
+    display: block;
+  }
+
+  .yellow {
+    background-image: yellow;
+    grid-area: yellow;
+  }
+
+  .orange {
+    background-color: transparent;
+    grid-area: orange;
+  }
+
+  footer {
+    background-color: transparent;
+    grid-area: footer;
+  }
+
+  .logo {
+    height: 150px;
+    width: 200px;
+    margin-bottom: 10px;
+    font-size: 80px;
+    font-family: 'Rubik Pixels', cursive;
+  }
+</style>
+ -->
+ <body>
+  <a href="../../index.html">home</a>
+  <div id="wrapper">
+      <header><div id="logo">Internet</div></header>
+      <nav>Das Wort Internet stammt von dem englischen Begriff internetwork was so viel bedeutet wie weltweites Netzwerk. Dieses Netzwerk besteht aus vielen miteinander verbundenen Computern, die Datenaustausch betreiben können.</nav>
+      <aside>Das Internet ist ein globales Netzwerk von Computern und anderen elektronischen Geräten, das es Benutzern auf der ganzen Welt ermöglicht, Informationen und Daten auszutauschen und miteinander zu kommunizieren. Es besteht aus einer Vielzahl von Netzwerken, die miteinander verbunden sind und es Benutzern ermöglichen, auf Websites, E-Mail, soziale Medien, Online-Streaming-Dienste und vieles mehr zuzugreifen. Das Internet hat die Art und Weise, wie Menschen kommunizieren, Geschäfte tätigen und Informationen teilen, revolutioniert.</aside>
+      <div id="green"></div>
+      <div id="yellow"></div>
+      <div id="orange"> <img src="Internet4.jpg" ></div>
+     
+      <footer></footer>
+  </div>
+</body>
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Rubik+Pixels&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Mono&display=swap');
+
+
+@font-face {
+    font-family:'RubikPixels-Regular' ;
+    src: url(fonts/RubikPixels-Regular.woff);
+}
+
+@font-face {
+    font-family: 'IBM Plex Mono', monospace;
+    src: url(fonts/SF-Mono-Regular.woff);
+}
+
+
+
+
+
+* {
+
+    box-sizing: border-box;
+    font-family: 'IBM Plex Mono', monospace;
+    background-color: rgb(255, 255, 255);
+    
+
+}
+
+a{
+    
+    color: rgb(0, 0, 0);
+    border-radius: 4px;
+    position: relative;
+    top: 2em;
+    right: 100px;
+
+    margin: auto;
+    position: fixed;
+}
+
+*
+#wrapper{
+    display: grid;
+    width: 800px;
+    gap: 40px;
+    margin: auto;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    grid-template-rows: 75px 80px 300px 100px 150px 50px;
+    grid-template-areas: 
+        "header header header header"
+        "nav nav nav nav"
+        "aside aside orange orange"
+        "aside aside orange orange"
+        "footer footer footer footer"
+
+
+    ; 
+}
+
+header{
+    background-color: transparent  ;
+    grid-area: header;
+
+}
+nav{
+    background-color: transparent;
+    grid-area: nav;
+    display: flex;
+}
+aside{
+    background-color: transparent;
+    grid-area: aside;
+    display: block;
+
+}
+
+#yellow{
+    background-image: yellow;
+    grid-area: yellow;
+    
+
+}
+
+#orange{
+    background-color: transparent;
+    grid-area: orange;
+    
+}
+
+footer{
+    background-color:transparent;
+    grid-area: footer;
+}
+
+#logo {
+    height: 150px;
+    width: 200px;
+    margin-bottom: 10px;
+    font-size: 80px;
+    font-family: 'Rubik Pixels', cursive;
+}
+
+
+
+
+
 </style>
